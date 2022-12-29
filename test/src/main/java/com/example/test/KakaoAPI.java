10402c721ababc4d44158ab7b6bb4af185d3ea84
Minhyeok Lee <minhyeok51@naver.com>
2022-12-29 23:43:29 +0900

    카카오로그인

---
+++ test/src/main/java/com/example/test/KakaoAPI.java
@@ -0,0 +1,130 @@
+package com.example.test;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class KakaoAPI {
+
+	public String getAccessToken(String code) {
+		String accessToken = "";
+		String refreshToken = "";
+		String reqURL = "https://kauth.kakao.com/oauth/token";
+		
+		try {
+			URL url = new URL(reqURL);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("POST");
+			conn.setDoOutput(true);
+			
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+			StringBuilder sb = new StringBuilder();
+			sb.append("grant_type=authorization_code");
+			sb.append("&client_id=2fd104a3490e89ea6ed730f58aa5ef46");
+			sb.append("&redirect_uri=http://localhost:8080/login");
+			sb.append("&code="+code);
+			
+			bw.write(sb.toString());
+			bw.flush();
+			
+			int responseCode = conn.getResponseCode();
+			System.out.println("response code = " + responseCode);
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			
+			String line = "";
+			String result = "";
+			while((line = br.readLine())!=null) {
+				result += line;
+			}
+			System.out.println("response body="+result);
+			
+			JSONParser parser = new JSONParser();
+			JSONObject element = (JSONObject) parser.parse(result);
+			
+			accessToken = element.get("access_token").toString();
+			refreshToken = element.get("refresh_token").toString();
+			
+			br.close();
+			bw.close();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return accessToken;
+	}
+
+	
+	public HashMap<String, Object> getUserInfo(String accessToken) {
+		HashMap<String, Object> userInfo = new HashMap<String, Object>();
+		String reqUrl = "https://kapi.kakao.com/v2/user/me";
+		try {
+			URL url = new URL(reqUrl);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+			int responseCode = conn.getResponseCode();
+			System.out.println("responseCode =" + responseCode);
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			
+			String line = "";
+			String result = "";
+			
+			while((line = br.readLine()) != null) {
+				result += line;
+			}
+			System.out.println("response body ="+result);
+			
+			JSONParser parser = new JSONParser();
+			JSONObject element = (JSONObject) parser.parse(result);
+			
+			JSONObject properties = (JSONObject)element.get("properties");
+			JSONObject kakaoAccount = (JSONObject) element.get("kakao_account");
+			
+			String nickname = properties.get("nickname").toString();
+			String email = kakaoAccount.get("email").toString();
+			String image = properties.get("profile_image").toString();
+			
+			userInfo.put("nickname", nickname);
+			userInfo.put("email", email);
+			userInfo.put("image", image);
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return userInfo;
+	}
+
+
+	public void kakaoLogout(String accessToken) {
+		String reqURL = "http://kapi.kakao.com/v1/user/logout";
+		try {
+			URL url = new URL(reqURL);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+			int responseCode = conn.getResponseCode();
+			System.out.println("responseCode = " + responseCode);
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			
+			String result = "";
+			String line = "";
+			
+			while((line = br.readLine()) != null) {
+				result+=line;
+			}
+			System.out.println(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
